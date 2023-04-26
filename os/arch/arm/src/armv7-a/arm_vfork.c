2895b9c329296ba44593837368be357675dfe617
Tirtha-Samsung <biswas.t@samsung.com>
2023-04-26 10:45:07 +0530

    Fix review commemts for ARMV7-A porting
    Signed-off-by: Tirtha-Samsung <biswas.t@samsung.com>

--- os/arch/arm/src/armv7-a/arm_vfork.c
+++ os/arch/arm/src/armv7-a/arm_vfork.c
@@ -125,10 +125,10 @@ pid_t up_vfork(const struct vfork_s *context)
 
   /* Allocate and initialize a TCB for the child task. */
 
-  child = nxtask_setup_vfork((start_t)(context->lr & ~1));
+  child = task_vforksetup((start_t)(context->lr & ~1));
   if (!child)
     {
-      serr("ERROR: nxtask_setup_vfork failed\n");
+      serr("ERROR: task_vforksetup failed\n");
       return (pid_t)ERROR;
     }
 
@@ -250,5 +250,5 @@ pid_t up_vfork(const struct vfork_s *context)
    * will discard the TCB by calling nxtask_abort_vfork().
    */
 
-  return nxtask_start_vfork(child);
+  return task_vforkstart(child);
 }
